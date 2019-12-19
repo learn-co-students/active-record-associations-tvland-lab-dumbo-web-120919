58993885a94c34403d99bf79553718bc628d5b45
tdonovan79 <tdonovan79@gmail.com>
2019-12-19 15:16:59 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+    #returns array of all actors associated with show
+    def actors_list
+        self.actors.map(&:full_name)
+    end
 end
